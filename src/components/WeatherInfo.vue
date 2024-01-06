13947d29e950db0204492b10a6da1c0458c27105
csslick <digitone@naver.com>
2024-01-07 00:48:29 +0900

    온도정보

--- src/components/WeatherInfo.vue
+++ src/components/WeatherInfo.vue
@@ -1,9 +1,16 @@
 <template>
     <div class="weather-info">
-      <div class="icon">{{ props.weather.icon }}</div>
-      <div class="temp">{{ props.weather.temp }}</div>
+      <div class="icon">
+        <img 
+          :src="`https://openweathermap.org/img/wn/${props.weather.icon}@2x.png`" 
+          :alt="`${weather.text}`">
+      </div>
+      <div class="temp">{{ (props.weather.temp - 273.15).toFixed(1) }}&deg;</div>
       <div class="text">{{ props.weather.text }}</div>
-      <div class="location">{{ props.weather.location }}</div>
+      <div class="location">
+        {{ props.weather.city }},
+        {{ props.weather.location }}
+      </div>
     </div>
 </template>
   
@@ -21,6 +28,9 @@
       margin-bottom: 20px;
       padding: 20px;
       border: 1px solid #ccc;
+    }
+    .temp {
+      font-size: 2em;
     }
   }
 </style>
